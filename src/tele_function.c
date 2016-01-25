e123cc44ea0d2a265417b0532f411f6240bfb813
htckenny <htckenny@hotmail.com>
2016-01-25 15:27:08 +0800

    rename tele_function

---
+++ src/tele_function.c
@@ -0,0 +1,618 @@
+#include <util/timestamp.h>
+#include <freertos/FreeRTOS.h>
+#include <dev/i2c.h>
+#include <util/hexdump.h>
+#include <string.h>
+#include <nanomind.h>
+#include <csp/csp_endian.h>
+#include <time.h>
+
+#include "subsystem.h"
+#include "parameter.h"
+#include "crc16.h"
+#include "tele_function.h"
+#include "fs.h"
+#include "task_SEUV.h"
+
+
+int tx_mode(uint8_t mode) {
+
+	if (mode == 1)
+		printf("Set COM to Normal Mode \r\n");
+	else if (mode == 0)
+		printf("Set COM to external telemetry mode \r\n");
+	else if (mode == 3)
+		printf("Set COM to receive loopback mode \r\n");
+
+	uint8_t txdata[2];
+	txdata[0] = com_tx_mode;
+	txdata[1] = mode;
+	if ( i2c_master_transaction_2(0, com_tx_node, &txdata, 2, 0, 0, com_delay) == E_NO_ERR) {
+		return 0;
+	}
+	else
+		return 1;
+}
+
+int set_tx_rate(uint8_t mode) {
+	/*
+	mode =1, rate=1200
+	mode =8, rate=9600
+
+	*/
+	uint8_t txbuf[2];
+	txbuf[0] = com_tx_rate;
+	txbuf[1] = mode;
+	if (i2c_master_transaction_2(0, com_tx_node, &txbuf, 2, 0, 0, com_delay) == E_NO_ERR)
+		return No_Error;
+	else
+		return Error;
+
+}
+void set_Call_Sign(int SSID) {
+
+
+	uint8_t txdata[8];
+	txdata[0] = 0x22; 	// set TO Call-Sign //
+	txdata[1] = 78; 	// NCKUGS
+	txdata[2] = 67;
+	txdata[3] = 75;
+	txdata[4] = 85;
+	txdata[5] = 71;
+	txdata[6] = 83;
+	txdata[7] = 0x60 + 2 * (uint8_t)SSID;
+
+	i2c_master_transaction_2(0, com_tx_node, &txdata, 8, 0, 0, com_delay);
+
+	txdata[0] = 0x23; 	// set FROM Call-Sign //
+	txdata[1] = 84; 	// TW01TN
+	txdata[2] = 87;
+	txdata[3] = 48;
+	txdata[4] = 49;
+	txdata[5] = 84;
+	txdata[6] = 78;
+	txdata[7] = 0x60 + 2 * (uint8_t)SSID;
+
+	i2c_master_transaction_2(0, com_tx_node, &txdata, 8, 0, 0, com_delay);
+
+
+}
+
+// Generates sequenceCount , reset if overflow
+uint8_t TC_Count() {
+
+	if (parameters.tc_count == 4)
+		parameters.tc_count = 0;
+
+	parameters.tc_count = parameters.tc_count + 1;
+	return parameters.tc_count - 1;
+}
+
+// Generates sequenceCount , reset if overflow
+uint8_t AX25_Sequence_Count() {
+	if (parameters.ax25_sequence_count == 255) {
+		parameters.ax25_sequence_count = 0;
+		return parameters.ax25_sequence_count;
+	}
+	parameters.ax25_sequence_count =
+	    parameters.ax25_sequence_count + 1;
+	return parameters.ax25_sequence_count;
+}
+
+
+
+// Generates sequenceCount , reset if overflow
+uint16_t CCSDS_GetSequenceCount(uint16_t apid) {
+	/* OBC related command */
+	if (apid == obc_apid) {
+		if (parameters.obc_packet_sequence_count == 16383) {
+			parameters.obc_packet_sequence_count = 0;
+		}
+		else
+			parameters.obc_packet_sequence_count ++ ;
+		return parameters.obc_packet_sequence_count;
+	}
+	/* OBC related command */
+	// else if (apid == inms_apid) {
+	// 	if (parameters.inms_packet_sequence_count == 16383) {
+	// 		parameters.inms_packet_sequence_count = 0;
+	// 		return parameters.inms_packet_sequence_count;
+	// 	}
+	// 	parameters.inms_packet_sequence_count =
+	// 	    parameters.inms_packet_sequence_count + 1;
+	// 	return parameters.inms_packet_sequence_count;
+	// }
+
+	// else if (apid == seuv_apid) {   //obc related command
+	// 	if (parameters.seuv_packet_sequence_count == 16383) {
+	// 		parameters.seuv_packet_sequence_count = 0;
+	// 		return parameters.seuv_packet_sequence_count;
+	// 	}
+	// 	parameters.seuv_packet_sequence_count =
+	// 	    parameters.seuv_packet_sequence_count + 1;
+	// 	return parameters.seuv_packet_sequence_count;
+	// }
+
+	// else if (apid == wod_apid) {   //obc related command
+	// 	if (parameters.wod_packet_sequence_count == 16383) {
+	// 		parameters.wod_packet_sequence_count = 0;
+	// 		return parameters.wod_packet_sequence_count;
+	// 	}
+	// 	parameters.wod_packet_sequence_count =
+	// 	    parameters.wod_packet_sequence_count + 1;
+	// 	return parameters.wod_packet_sequence_count;
+	// }
+
+	// else if (apid == phoenix_hk_apid) {   //obc related command
+	// 	if (parameters.phoenix_hk_packet_sequence_count == 16383) {
+	// 		parameters.phoenix_hk_packet_sequence_count = 0;
+	// 		return parameters.phoenix_hk_packet_sequence_count;
+	// 	}
+	// 	parameters.phoenix_hk_packet_sequence_count =
+	// 	    parameters.phoenix_hk_packet_sequence_count + 1;
+	// 	return parameters.phoenix_hk_packet_sequence_count;
+	// }
+	else
+		return 255;
+}
+
+// Generates a complete telemetry packet with the values and data specified
+uint8_t CCSDS_GenerateTelemetryPacketWithTime(uint8_t* telemetryBuffer,
+        uint8_t* telemetryBufferSize, uint16_t apid, uint8_t serviceType,
+        uint8_t serviceSubtype, uint8_t* sourceData, uint8_t sourceDataLength,
+        uint32_t time) {
+
+	uint16_t chk;   					// CRC syndrome
+	uint16_t sequenceCount;   			// Sequence Count of packet
+	uint8_t packetLength;   			// Total length of packet
+
+	uint16_t packetLengthFieldValue;  	// Value of Packet Length header field
+
+	uint8_t *dataPtr;  					// For source data copy
+	uint8_t *dataEndPtr;
+
+	if (sourceData == NULL)
+		return ERR_MISSING_PARAMETER; 	// No data
+	if (telemetryBuffer == NULL)
+		return ERR_MISSING_PARAMETER; 	// No telemetry buffer
+	if (telemetryBufferSize == NULL)
+		return ERR_MISSING_PARAMETER; 	// No telemetry buffer size
+
+	// Total packet length must be <= than 235 octets (otherwise it cannot be transmitted in COM board)
+	if (sourceDataLength > (235 - TM_NONDATA_SIZE - AX25_2ed_size - 1))
+		return ERR_CCSDS_TOO_MUCH_DATA; // Data too big
+
+	// Total packet length (not value of packet length field !)
+	packetLength = TM_NONDATA_SIZE + sourceDataLength;
+
+	// Test if telemetry buffer big enough
+	if (*telemetryBufferSize < packetLength)
+		return ERR_CCSDS_BUFFER_TOO_SMALL; // Buffer too small
+
+	// Get sequence count
+	sequenceCount = CCSDS_GetSequenceCount(apid);
+
+	// Packet Header (Packet ID, Packet Sequence Control and Packet Length) (48 bits)
+	// Packet ID (16 bits)
+	telemetryBuffer[0] = 0x08 | ((uint8_t)(apid >> 8) & 0x07);
+	// Version number (3 bits) = 0, Type (1bit) = 0, Data Field Header Flag (1 bit) = 1, APID (11 bits) -> 3 MSB
+	telemetryBuffer[1] = (uint8_t)(apid);   // APID (11 bits) -> 8 LSB
+	// Packet Sequence Control (16 bits)
+	telemetryBuffer[2] = 0xC0 | ((uint8_t)(sequenceCount >> 8) & 0x3F);
+	// Grouping Flag (2 bits), Source Sequence Count (14 bits) -> 6 MSB
+
+	telemetryBuffer[3] = (uint8_t)(sequenceCount);
+	// Source Sequence  Count (14 bits) -> 8 LSB
+
+	// Packet Length (16 bits)
+	packetLengthFieldValue = packetLength - 7;
+	telemetryBuffer[4] = (uint8_t)(packetLengthFieldValue >> 8); 	// Packet Length (16 bits) 	-> 8 MSB
+	telemetryBuffer[5] = (uint8_t)(packetLengthFieldValue); 		// 							-> 8 LSB
+
+	// Packet Data Field (Telemetry Data Field Header, Source Data and Packet Error Control) (Variable)
+	// Telemetry Data Field Header (56 bits)
+	telemetryBuffer[6] = 0x10; 				// Spare (1 bit) = 0, PUS Version Number (3 bits) = 1, Spare (4 bits) = 0
+	telemetryBuffer[7] = serviceType; 		// Service Type (8 bits)
+	telemetryBuffer[8] = serviceSubtype; 	// Service Subtype (8 bits)
+
+	// Absolute Time (40 bits, 5 octets)
+	// CUC with 4 octets of coarse time and 1 octet of fine time
+	telemetryBuffer[9] = (uint8_t)(time >> 24); 		// C1
+	telemetryBuffer[10] = (uint8_t)(time >> 16); 		// C2
+	telemetryBuffer[11] = (uint8_t)(time >> 8); 		// C3
+	telemetryBuffer[12] = (uint8_t)(time); 				// C4
+	telemetryBuffer[13] = 0; 							// F1 // TODO: Seems to be always 0
+
+	// Copy Source Data (variable length)
+	dataPtr = (telemetryBuffer + TM_HEADERS_SIZE);
+	dataEndPtr = dataPtr + sourceDataLength;
+	while (dataPtr < dataEndPtr)
+		*(dataPtr++) = *(sourceData++);
+
+	// Packet Error Control (16 bits)
+	chk = 0xFFFF; 	// Init syndrome
+
+	unsigned int LTbl[256];
+	InitLtbl(LTbl);
+
+	// Compute CRC (all packet data except FCS field)
+	for (int i = 0; i < packetLength - 2; i++)
+		chk = crc_opt(telemetryBuffer[i], chk, LTbl);
+
+
+
+	// Fill CRC field
+	telemetryBuffer[packetLength - 2] = (uint8_t)(chk >> 8);
+	telemetryBuffer[packetLength - 1] = (uint8_t)(chk);
+
+	// Return number of bytes written
+	*telemetryBufferSize = packetLength;
+
+	return ERR_SUCCESS;
+}
+
+
+
+// Generates a complete telemetry packet with the values and data specified
+uint8_t CCSDS_GenerateTelemetryPacket(uint8_t* telemetryBuffer,
+                                      uint8_t* telemetryBufferSize, uint16_t apid, uint8_t serviceType,
+                                      uint8_t serviceSubtype, uint8_t* sourceData, uint8_t sourceDataLength) {
+	uint32_t time;
+	timestamp_t t;
+	t.tv_sec = 0;
+	t.tv_nsec = 0;
+	obc_timesync(&t, 6000);
+	time = t.tv_sec - 946684800;
+
+	return CCSDS_GenerateTelemetryPacketWithTime(telemetryBuffer,
+	        telemetryBufferSize, apid, serviceType, serviceSubtype, sourceData,
+	        sourceDataLength, time);
+}
+
+
+//packet with AX.25 Secondary header and Send to COM
+uint8_t AX25_GenerateTelemetryPacket_Send(uint8_t* data , uint8_t data_len) {
+
+	uint8_t txBuffer[255];
+	uint8_t txlen;
+	uint8_t rx;
+	if (data_len > i2c_max_size - 6)
+		return ERR_SIZE_ERROR;
+
+	memcpy(&txBuffer[5], data, data_len);
+	txBuffer[0] = com_tx_send;
+	txBuffer[1] = 0; 						// Frame Identification(8bits)
+	txBuffer[2] = AX25_Sequence_Count(); 	// Master Frame Count
+	txBuffer[3] = txBuffer[2];				// Virtual Channel Frame Count
+	txBuffer[4] = 0x00; 					// First Header Point : 0xFE = no packet fragment inside
+
+	txBuffer[data_len + AX25_2ed_size] = TC_Count();
+	txlen = data_len + AX25_2ed_size + 1;
+	if (parameters.shutdown_flag != 1) {
+		if (i2c_master_transaction_2(0, com_tx_node, &txBuffer, txlen, &rx, 1, com_delay) != E_NO_ERR)
+			return Error;
+	}
+	else
+		printf("transmitter shutdown!!");
+
+	return ERR_SUCCESS;
+}
+
+
+uint8_t AX25_Send(uint8_t* data , uint8_t data_len) {
+
+	uint8_t txBuffer[255];
+	uint8_t txlen;
+	uint8_t rx;
+	if (data_len > i2c_max_size - 1)
+		return ERR_SIZE_ERROR;
+
+	memcpy(&txBuffer[1], data, data_len);
+	txBuffer[0] = com_tx_send;
+	txlen = data_len + 1;
+	if (parameters.shutdown_flag != 1) {
+		if (i2c_master_transaction_2(0, com_tx_node, &txBuffer, txlen, &rx, 1, com_delay) != E_NO_ERR)
+			return Error;
+	}
+
+	return ERR_SUCCESS;
+}
+
+/*-----------------------------------------------
+ * sendTelecommandReport_Success()
+ * -----------------------------------------------
+ * Input :
+ * 	*telecommand : buffer containing the concerned telecommand packet
+ *  reportType : contains the success type (received, completed, progress, etc)
+ *
+ * Output :
+ *
+ *	error : No_Error when no error has occured, and error ID otherwise
+ *
+ * Description:
+ * 	This function is called by decodeService8() every time a success message
+ *  needs to be downlinked to the ground station in a telemetry packet, after
+ *  a successful step in the execution of a service 8 function.
+ *
+ * -----------------------------------------------*/
+uint8_t sendTelecommandReport_Success(uint8_t * telecommand, uint8_t reportType) {
+
+	uint8_t err;
+	// CCSDS Source Data
+	uint8_t success[TM_S1_SUCCESS_SIZE];
+	// CCSDS Packet length
+	uint8_t packetLength = TM_NONDATA_SIZE + TM_S1_SUCCESS_SIZE;
+
+	uint8_t temporaryBuffer[packetLength];
+
+	// Packet Sequence Control (direct copy from telecommand)
+	success[0] = telecommand[0];
+	success[1] = telecommand[1];
+
+	// Telecommand Packet ID (direct copy from telecommand)
+	success[2] = telecommand[2];
+	success[3] = telecommand[3];
+
+	// Generate CCSDS telemetry packet
+	err = CCSDS_GenerateTelemetryPacket(&temporaryBuffer[0], &packetLength,
+	                                    obc_apid, CCSDS_T1_TELECOMMAND_VERIFICATION, reportType, success,
+	                                    TM_S1_SUCCESS_SIZE);
+
+	if (err == ERR_SUCCESS) {
+		return AX25_GenerateTelemetryPacket_Send(&temporaryBuffer[0] , packetLength);
+	}
+	return err;
+}
+uint8_t sendTelecommandReport_Success_INMS(uint8_t * telecommand, uint8_t reportType) {
+
+	uint8_t err;
+	// CCSDS Source Data
+	uint8_t success[TM_S1_SUCCESS_SIZE];
+	// CCSDS Packet length
+	uint8_t packetLength = TM_NONDATA_SIZE + TM_S1_SUCCESS_SIZE;
+
+	uint8_t temporaryBuffer[packetLength];
+
+	// Packet Sequence Control (direct copy from telecommand)
+	success[0] = telecommand[0];
+	success[1] = telecommand[1];
+
+	// Telecommand Packet ID (direct copy from telecommand)
+	success[2] = telecommand[2];
+	success[3] = telecommand[3];
+
+	// Generate CCSDS telemetry packet
+	err = CCSDS_GenerateTelemetryPacket(&temporaryBuffer[0], &packetLength,
+	                                    inms_apid, CCSDS_T1_TELECOMMAND_VERIFICATION, reportType, success,
+	                                    TM_S1_SUCCESS_SIZE);
+
+	if (err == ERR_SUCCESS) {
+		return AX25_GenerateTelemetryPacket_Send(&temporaryBuffer[0] , packetLength);
+	}
+	return err;
+}
+/*-----------------------------------------------
+ * sendTelecommandReport_Failure()
+ * -----------------------------------------------
+ * Input :
+ * 	*telecommand : buffer containing the concerned telecommand packet
+ * 	reportType : contains the success type (received, completed, progress, etc)
+ *  err : describes the nature of the failure
+ *
+ * Output :
+ *	none
+ *
+ * Description:
+ * 	This function is called by decodeService8() every time a failure message
+ *  needs to be downlinked to the ground station in a telemetry packet, after
+ *  an unsuccesful step in the execution of a service 8 function.
+ *
+ * -----------------------------------------------*/
+
+// Sends a telecommand failure report
+// Report type can be any failure subtype of service type 1
+// Returns encountered errors
+uint8_t sendTelecommandReport_Failure(uint8_t* telecommand, uint8_t reportType, uint8_t err)
+{
+	// CCSDS Source Data
+	uint8_t failure[TM_S1_FAILURE_SIZE];
+	// CCSDS Packet length
+	uint8_t packetLength = TM_NONDATA_SIZE + TM_S1_FAILURE_SIZE;
+
+	uint8_t temporaryBuffer[packetLength];
+
+	// Packet Sequence Control (direct copy from telecommand)
+	failure[0] = telecommand[0];
+	failure[1] = telecommand[1];
+
+	// Telecommand Packet ID (direct copy from telecommand)
+	failure[2] = telecommand[2];
+	failure[3] = telecommand[3];
+
+	// Error Code
+	failure[4] = 0x00;
+	failure[5] = err;
+
+	// Generate CCSDS telemetry packet
+	err = CCSDS_GenerateTelemetryPacket(&temporaryBuffer[0], &packetLength,
+	                                    obc_apid, CCSDS_T1_TELECOMMAND_VERIFICATION, reportType, failure,
+	                                    TM_S1_FAILURE_SIZE);
+
+	if (err == ERR_SUCCESS)
+		return AX25_GenerateTelemetryPacket_Send(&temporaryBuffer[0], packetLength);
+
+
+	return err;
+}
+uint8_t sendTelecommandReport_Failure_INMS(uint8_t* telecommand, uint8_t reportType, uint8_t err)
+{
+	// CCSDS Source Data
+	uint8_t failure[TM_S1_FAILURE_SIZE];
+	// CCSDS Packet length
+	uint8_t packetLength = TM_NONDATA_SIZE + TM_S1_FAILURE_SIZE;
+
+	uint8_t temporaryBuffer[packetLength];
+
+	// Packet Sequence Control (direct copy from telecommand)
+	failure[0] = telecommand[0];
+	failure[1] = telecommand[1];
+
+	// Telecommand Packet ID (direct copy from telecommand)
+	failure[2] = telecommand[2];
+	failure[3] = telecommand[3];
+
+	// Error Code
+	failure[4] = 0x00;
+	failure[5] = err;
+
+	// Generate CCSDS telemetry packet
+	err = CCSDS_GenerateTelemetryPacket(&temporaryBuffer[0], &packetLength,
+	                                    inms_apid, CCSDS_T1_TELECOMMAND_VERIFICATION, reportType, failure,
+	                                    TM_S1_FAILURE_SIZE);
+
+	if (err == ERR_SUCCESS)
+		return AX25_GenerateTelemetryPacket_Send(&temporaryBuffer[0], packetLength);
+
+
+	return err;
+}
+
+
+uint8_t SendPacketWithCCSDS_AX25(void * hkBuffer, uint8_t hkBufferLength, uint8_t apid, uint8_t type, uint8_t subTypes) {
+	uint8_t err;
+	uint8_t packetLength = 255;
+
+	uint8_t messageBuffer[255];
+
+
+	err = CCSDS_GenerateTelemetryPacket(&messageBuffer[0], &packetLength, apid, type, subTypes, hkBuffer, hkBufferLength);
+	if (err == ERR_SUCCESS) {
+		return AX25_GenerateTelemetryPacket_Send(&messageBuffer[0], packetLength);
+//   	 return AX25_Send(&messageBuffer[0],packetLength);
+	}
+	return CCSDS_PACKET_ERROR;
+}  /* end of SendPacketWithCCSDS_AX25 */
+
+
+uint8_t SendDataWithCCSDS_AX25(uint8_t datatype, uint8_t* data) { //add sid then packet it with ccsds
+	/*   data type:
+	 *   1 = HK
+	 *   2 = inms
+	 *   3 = seuv
+	 *   4 = eop
+	 *   5 = wod          */
+	uint8_t datalength;
+	uint8_t databuffer[250];
+	uint8_t txframe[246];
+	uint8_t tx_length = 245;
+	uint8_t err;
+
+	if (datatype == 1) {
+		databuffer[0] = phoenix_hk_sid;
+		datalength = hk_length + 1;
+	}
+	else if (datatype == 2) {
+		databuffer[0] = inms_sid;
+		datalength = inms_data_length + 1;
+	}
+	else if (datatype == 3) {
+		databuffer[0] = seuv_sid;
+		datalength = seuv_length + 1;
+	}
+	else if (datatype == 4) {
+		databuffer[0] = eop_sid;
+		datalength = eop_length + 1;
+	}
+	else if (datatype == 5) {
+
+		databuffer[0] = wod_sid;
+
+		databuffer[1] = 1; //the head part of the WOD packet,
+		memcpy(&databuffer[2], data, 150); //the first 150 byte of WOD data
+		err = CCSDS_GenerateTelemetryPacket(&txframe[0], &tx_length, obc_apid, 15, 9, &databuffer[0], 152);
+		if (err == ERR_SUCCESS)
+			AX25_GenerateTelemetryPacket_Send(&txframe[0], tx_length);
+		else
+			return Error;
+
+		databuffer[1] = 2; //the last part of the WOD packet,
+		memcpy(&databuffer[2], data + 150, 82); //the rest 82 byte of WOD data
+		err = CCSDS_GenerateTelemetryPacket(&txframe[0], &tx_length, obc_apid, 15, 9, &databuffer[0], 152);
+		if (err == ERR_SUCCESS)
+			AX25_GenerateTelemetryPacket_Send(&txframe[0], tx_length);
+		else
+			return Error;
+
+
+		return No_Error;
+
+	}
+	else
+		return Error;
+
+	memcpy(&databuffer[1], data, datalength - 1); //copy data to databuffer
+
+	err = CCSDS_GenerateTelemetryPacket(&txframe[0], &tx_length, obc_apid, 15, 9, &databuffer[0], datalength);
+
+	if (err == ERR_SUCCESS) {
+		return AX25_GenerateTelemetryPacket_Send(&txframe[0], tx_length);
+//  return AX25_Send(&txframe[0],tx_length);
+	} else
+		printf("have error on generate CCSDS packet, maybe overflow  , tx_length = %d \n", tx_length);
+
+	return Error;
+}
+
+
+void decodeCCSDS_Command(uint8_t * telecommand, uint8_t packet_length) {
+
+	uint8_t serviceType =  telecommand[7];
+	uint8_t serviceSubType = telecommand[8];
+	uint16_t chk;
+	unsigned int LTbl[256];
+
+	// Compute CRC (all packet data except FCS field)
+	chk = 0xFFFF;
+
+	InitLtbl(LTbl);
+	for (int i = 0; i < packet_length - 2; i++)
+		chk = crc_opt(telecommand[i], chk, LTbl);
+
+	printf("crc check performing (testing) \n");
+	// Check CRC field
+	if (telecommand[packet_length - 2] == (uint8_t)(chk >> 8) && telecommand[packet_length - 1] == (uint8_t)(chk) ) {
+		printf("Telecommand Pass CRC Check (Has CRC check now)\n");
+
+		switch (serviceType) {
+
+		case T3_SYS_CONF:
+			decodeService3(serviceSubType, telecommand);
+			break;
+		case T8_function_management:
+			decodeService8(serviceSubType, telecommand);
+			break;
+		case T11_OnBoard_Schedule:
+			decodeService11(serviceSubType, telecommand);
+			break;
+		case T13_LargeData_Transfer:
+			decodeService13(serviceSubType, telecommand);
+			break;
+		case T15_dowlink_management:
+			decodeService15(serviceSubType, telecommand);
+			break;
+		case T131_ADCS:
+			decodeService131(serviceSubType, telecommand);
+			break;
+		case  T132_SEUV:
+			decodeService132(serviceSubType, telecommand);
+			break;
+		default:
+			sendTelecommandReport_Failure(telecommand, CCSDS_T1_ACCEPTANCE_FAIL, CCSDS_ERR_ILLEGAL_TYPE);
+
+			break;
+		}
+	}
+	else {
+		printf(" CRC Check not pass!!!\n");
+	}
+}  /* end of decodeCCSDS_Command */
---
+++ src/tele_function.h
@@ -0,0 +1,138 @@
+/*
+ * function.h
+ *
+ *  Created on: 2015/1/29
+ *      Author: rusei
+ */
+
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+
+uint8_t CCSDS_GenerateTelemetryPacket(uint8_t* telemetryBuffer,uint8_t* telemetryBufferSize, uint16_t apid, uint8_t serviceType,uint8_t serviceSubtype, uint8_t* sourceData, uint8_t sourceDataLength);
+uint8_t sendTelecommandReport_Success(uint8_t* telecommand, uint8_t reportType);
+uint8_t sendTelecommandReport_Success_INMS(uint8_t * telecommand, uint8_t reportType);
+uint8_t sendTelecommandReport_Failure(uint8_t* telecommand, uint8_t reportType,uint8_t err);
+uint8_t sendTelecommandReport_Failure_INMS(uint8_t* telecommand, uint8_t reportType,uint8_t err);
+void decodeCCSDS_Command(uint8_t * telecommand, uint8_t packet_length);
+uint8_t SendDataWithCCSDS_AX25(uint8_t datatype,uint8_t* data);
+uint8_t SendPacketWithCCSDS_AX25(void * hkBuffer,uint8_t hkBufferLength,uint8_t apid,uint8_t type,uint8_t subTypes);
+int tx_mode(uint8_t mode);
+void set_Call_Sign(int SSID);
+int set_tx_rate(uint8_t mode);
+
+
+void decodeService3(uint8_t subType, uint8_t*telecommand);
+void decodeService8(uint8_t subType, uint8_t*telecommand);
+void decodeService11(uint8_t subType, uint8_t*telecommand);
+void decodeService13(uint8_t subType, uint8_t*telecommand);
+void decodeService15(uint8_t subType, uint8_t*telecommand);
+void decodeService131(uint8_t subType, uint8_t*telecommand);
+void decodeService132(uint8_t subType, uint8_t*telecommand);
+
+
+
+/*Type 129 ADCS Telecommand Table*/
+#define Reset 1
+#define Set_Unix_Time 2
+#define ADCS_Run_Mode 3
+#define Selected_Logged_Data 4
+#define Power_Control 5
+#define Deploy_Magnetometer_Boom 6
+#define Trigger_ADCS_Loop 7
+#define Set_Attitude_Estimation_Mode 17
+#define Set_Attitude_Control_Mode 18
+#define Set_Commanded_Attitude_Angles 19
+#define Set_Wheel_Speed 32
+#define Set_Magnetorquer_Output 33
+#define Set_Startup_Mode 102
+#define Set_SGP4_Orbit_Parameters 64
+#define Set_Configuration 80
+#define Set_Magnetorquer_Configuration 81
+#define Set_Wheel_Configuration 82
+#define Set_CSS_Configuration 83
+#define Set_Sun_Sensor_Configuration 84
+#define Set_Nadir_Sensor_Configuration 85
+#define Set_Magnetometer_Configuration 86
+#define Set_Rate_Sensor_Configuration 87
+#define Set_Detumbling_Control_Parameters 88
+#define Set_Ymomentum_Control_Parameters 89
+#define Set_Moment_Of_Inertia 90
+#define Set_Estimation_Parameters 91
+#define Save_Configuration 100
+#define Save_Orbit_Parameters 101
+#define Capture_And_Save_Image 110
+#define Reset_File_List 114
+#define Advance_File_List_Index 115
+#define Initialize_File_Download 116
+#define Advance_File_Read_Pointer 117
+#define Erase_File 118
+#define Erase_All_Files 119
+#define Set_Boot_Index 120
+#define Erase_Program 121
+#define Upload_Program_Block 122
+#define Finalize_Program_Upload 123
+
+
+/*ADCS Telemetry Table*/
+#define Identification 128
+#define Communication_Status 130
+#define Telecommand_Acknowledge 131
+#define Reset_Cause 133
+#define Actuator_Commands 138
+#define ACP_Execution_State 189
+#define ACP_Execution_Times 190
+#define EDAC_And_Latchup_Counters 193
+#define Startup_Mode 194
+#define File_Information 240
+#define File_Block_CRC 241
+#define File_Data_Block 242
+#define Power_Control_Selection 134
+#define Power_And_Temperature_Measurements 135
+#define ADCS_State 136
+#define ADCS_Measurements 137
+#define Current_Time 143
+#define Current_State 144
+#define Estimated_Attitude_Angles 145
+#define Estimated_Angular_Rates 146
+#define Satellite_Position_LLH 147
+#define Satellite_Velocity_ECI 148
+#define Magnetic_Field_Vector 149
+#define Coarse_Sun_Vector 150
+#define Fine_Sun_Vector 151
+#define Nadir_Vector 152
+#define Rate_Sensor_Rates 153
+#define Wheel_Speed 154
+#define CubeSense_Current_Measurements 173
+#define CubeControl_Current_Measurements 174
+#define Peripheral_Current_And_Temperature_Measurements 175
+#define Raw_Sensor_Measurements 139
+#define Angular_Rate_Covariance 163
+#define Raw_Nadir_Sensor 164
+#define Raw_Sun_Sensor 165
+#define Raw_CSS 166
+#define Raw_Magnetometer 167
+#define Raw_GPS_Status 168
+#define Raw_GPS_Time 169
+#define Raw_GPS_X 170
+#define Raw_GPS_Y 171
+#define Raw_GPS_Z 172
+#define Estimation_Data 140
+#define IGRF_Modelled_Magnetic_Field_Vector 157
+#define Modelled_Sun_Vector 158
+#define Estimated_Gyro_Bias 159
+#define Estimated_Innovation_Vector 160
+#define Quaternion_Error_Vector 161
+#define Quaternion_Covariance 162
+#define Magnetorquer_Command 155
+#define Wheel_Speed_Commands 156
+#define SGP4_Orbit_Parameters 191
+#define Configuration 192
+#define Status_Of_Image_Capture_And_Save_Operation 230
+#define Uploaded_Program_Status 250
+#define Get_Flash_Program_List 251
+
+
+/*--------------------------*/
+
+#endif /* FUNCTION_H_ */

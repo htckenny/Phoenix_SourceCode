78984eb2d6f2851c22345eaa631728d607c5e997
Kenny Huang <htckenny@hotmail.com>
2016-01-27 22:49:22 +0800

    remove test directory

--- src/test/I2C_GPIO.h
+++
@@ -1,6 +0,0 @@
-void i2c_mpio_init(void) ;
-void i2c_mpio_start(void);
-void i2c_mpio_stop(void) ;
-void i2c_mpio_write(unsigned char data) ;
-unsigned char i2c_mpio_read(unsigned char send_ack) ;
-int send_i2c_adcs(unsigned char reg1,unsigned char reg2, int rxNum);

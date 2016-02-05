cc032873fbbf959aec544b5a0323533a7c893dc5
Kenny Huang <htckenny@hotmail.com>
2016-02-05 10:48:41 +0800

    Add eop count in parameters

--- src/subsystem.c
+++ src/subsystem.c
@@ -77,11 +77,13 @@ int parameter_init() {
 	abort_transfer_flag 				= 0;
 	magnetometer_deploy					= 0;
 	HK_frame.sun_light_flag				= 0;
+
 	/*--File System store count--*/
 	parameters.wod_store_count			= 0;
 	parameters.inms_store_count			= 0;
 	parameters.seuv_store_count			= 0;
 	parameters.hk_store_count			= 0;
+	parameters.eop_store_count			= 0;
 
 	/* Protocol sequence count */
 	parameters.obc_packet_sequence_count = 0;

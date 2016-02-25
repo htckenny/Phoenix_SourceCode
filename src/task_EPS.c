d0b47f0a01ff0ec8c9bd23eb0b61a1e3fcd60f23
Kenny Huang <htckenny@hotmail.com>
2016-02-25 17:00:24 +0800

    Update of EPS task
    Add three more mode

--- src/task_EPS.c
+++ src/task_EPS.c
@@ -35,7 +35,6 @@ FILINFO *fno;
 int cleanfile = 0;
 char fileName1[30];
 extern int eps_write(uint8_t *frameCont, int choose);
-
 void EPS_Task(void * pvParameters)
 {
 	uint8_t uchar_eps[12]; // neccessary
@@ -43,34 +42,48 @@ void EPS_Task(void * pvParameters)
 	uint8_t txbuf[2];
 	txbuf[0] = 0x08; 			//ID = 8 EPS HK port
 	txbuf[1] = 0x00;
-	int i = 0, delay = 1, period = 0, rate[180], j = 0;
+	int i = 0, period = 0, rate[180], j = 0, ccv = 0;
+	double delay = 0 , time = 0;
+	portTickType start_time = 0, stop_time = 0;
 	char choose;
-	int ccv ;
-	uint16_t vboost3, vbatt = 0, current_in = 0, current_out, btemp, cursun = 0;
+	uint16_t vboost3 = 0, vbatt = 0, current_in = 0, current_out = 0, btemp, cursun = 0;
 	char time_unit[4];
+	unsigned int ms = 0;
 	// clear all memery space
 	for (i = 0 ; i < 12 ; i++)
 		uchar_eps[i] = 0;
-	// uint32_t seconds[] = {0};
 
-	printf("a. Record the closed circuit voltage \n");
-	printf("b. Charge cycle \n");
+	printf("a. Charge cycle \n"); //ASCII a=97
+	printf("b. Record the closed circuit voltage \n");
 	printf("c. Discharge cycle \n");
 	printf("d. Overcharge cycle \n");
 	printf("e. Discharge after overcharge cycle \n");
+	printf("f. Overdischarge cycle \n");
+	printf("g. Charge after overdischarge cycle or external battery short \n");
+	printf("h. External battery short \n");
 	printf("Point which test ");
 	scanf("%c", &choose);
 
 	switch (choose)
 	{
-	case 'a' :
+	case 'a':
+		delay = 60 * delay_time_based;
+		strcpy(time_unit, "min");
+		break;
+
+	case 'b' :
 		delay = 1 * delay_time_based;
 		ccv = 30;
 		strcpy(time_unit, "sec");
 		break;
-	case 'b':
-		delay = 60 * delay_time_based;
-		strcpy(time_unit, "min");
+
+	case 'g' :
+		delay = 1 * delay_time_based;
+		strcpy(time_unit, "sec");
+		break;
+
+	case 'h' :
+		strcpy(time_unit, "sec");
 		break;
 
 	default:
@@ -81,14 +94,22 @@ void EPS_Task(void * pvParameters)
 			delay = 60 * delay_time_based;
 			strcpy(time_unit, "min");
 		}
-		for (i = 0; i < 180; i++)
-			rate[i] = 0;
-		i = 0;
+
 	}
+
+	for (i = 0; i < 180; i++)
+		rate[i] = 0;
+	i = 0;
 
 	while (1)
 	{
-		printf(" %4d %s \n", i, time_unit);
+		ms = (stop_time - start_time) * (1000 / configTICK_RATE_HZ);
+		printf("\n %5.3f %s \n", time , time_unit);
+		start_time = xTaskGetTickCount();
+		if (choose == 104 && time > 10) // change the external battery short sample time
+			delay = 10;
+		if (choose == 104 && time > 1800)
+			break;
 		i++;
 		if (i2c_master_transaction_2(0, eps_node, &txbuf, 2, &rxbuf, 133, eps_delay) == E_NO_ERR)
 		{
@@ -126,55 +147,58 @@ void EPS_Task(void * pvParameters)
 			printf("Input battery current: %4" PRIu16 "\n", cursun);
 			printf("Output current: %4" PRIu16 "\n", current_out);
 			printf("Battery temperature: %4" PRIu16 "\n", btemp);
-
-			eps_write(uchar_eps, choose);			//write into SD card
-			vTaskDelay(delay);
 		}
 		else
 			printf("Error, cannot communicate with EPS\n");
-		if (i == ccv && choose == 97) // judge the charge current
+
+		eps_write(uchar_eps, choose);			//write into SD card
+
+		if (i == ccv && choose == 98) // Record ccv(closed circuit voltage)
 			break;
 
-		if (choose == 98 && cursun < 100) // wherether does the circumstance of the lower current maintent over 3 mins
+		if ((choose == 97 || choose == 103) && cursun < 100) // wherether does the circumstance of the lower current maintent over 3 mins
 			period++;
 
-		if (choose == 98 && cursun >= 100)
+		if ((choose == 97 || choose == 103) && cursun >= 100)
 			period = 0;
 
-		if (choose >= 99) //overcharge or discharge judge mechamism
+		if (choose > 98 && choose < 103) //overcharge or discharge ¡Boverdischarge judge mechamism
 		{
-			if (abs((vbatt - rate[j])) >= 0.005 * rate[j]) //wherether does the circumstance change
+			if (abs((vbatt - rate[j])) >= 0.005 * rate[j] && (vbatt - rate[j]) != 0) //wherether does the circumstance change
 				period = 0;
 			else
 				period++;
 
+			printf("vbatt:%d rate[%d]:%d period:%d\n", vbatt, j, rate[j], period);
 			rate[j] = vbatt;
 			j++;
 
-			if (j == 180)
+			if (j == 180 && choose > 99)
+				j = 0;
+			if (j == 3 && choose == 99) //wherether dose it reach the discharge cycle threshold
 				j = 0;
 		}
 
-		if (period > 3 && (choose == 98 || choose == 101)) // trigger the warning signal for charge
+		if (period > 3 && choose == 97) // trigger the warning signal for charge
 		{
 			delay = 10 * delay_time_based;
 			break;
 		}
 
-		if (period > 180 && choose >= 99) // trigger the warning signal for overcharge
+		if (period > 180 && choose > 99) // trigger the warning signal for overcharge
 		{
 			delay = 10 * delay_time_based;
 			break;
 		}
+		vTaskDelay(delay);
+		stop_time = xTaskGetTickCount();
 	}
 
-	if (period > 3 ) //warning signal
-		for (i = 0; i < 60; i++)
-		{
-			printf(" break time : %4d sec \n", i * 10);
-			vTaskDelay(delay);
-		}
-	/* End of init */
+	for (i = 0; i < 60; i++) //warning signal
+	{
+		printf(" break time : %4d sec \n", i * 10);
+		vTaskDelay(delay);
+	}
 	vTaskDelete(NULL);
 }
 
@@ -207,6 +231,21 @@ int eps_write(uint8_t *frameCont, int choose)
 			f_unlink("Battery/battery_e.bin");
 			strcpy(fileName1, "Battery/battery_e.bin");
 		}
+		if (choose == 102)
+		{
+			f_unlink("Battery/battery_f.bin");
+			strcpy(fileName1, "Battery/battery_f.bin");
+		}
+		if (choose == 103)
+		{
+			f_unlink("Battery/battery_g.bin");
+			strcpy(fileName1, "Battery/battery_g.bin");
+		}
+		if (choose == 104)
+		{
+			f_unlink("Battery/battery_h.bin");
+			strcpy(fileName1, "Battery/battery_h.bin");
+		}
 	}
 	cleanfile = 1;
 
@@ -232,7 +271,7 @@ int eps_write(uint8_t *frameCont, int choose)
 	res = f_write(&file, &frameCont[6], 2, &bw);
 	res = f_write(&file, &frameCont[8], 2, &bw);
 	res = f_write(&file, &frameCont[10], 2, &bw);
-	f_close(&file);
 
+	f_close(&file);
 	return No_Error;
 }

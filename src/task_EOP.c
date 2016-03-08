0c1f34ee7867e29eb5002bcd805a1d26c6570081
Kenny Huang <htckenny@hotmail.com>
2016-03-08 19:48:50 +0800

    Fix the bugs with EOP packet
    - Get correct frame from EPS
    - Move time bytes from fs to here
    - Power information will collect every 60 seconds

--- src/task_EOP.c
+++ src/task_EOP.c
@@ -22,21 +22,28 @@
 #include "subsystem.h"
 
 void EOP_Task(void * pvParameters) {
+	uint8_t counter = 0;
 	uint8_t uchar_eop[eop_length];
 	uint8_t rxbuf[48];
 	uint8_t txbuf[2];
 	uint16_t vbatt;
 	uint16_t cursun;
 	uint16_t cursys;
-	unsigned int p_sun;
-	unsigned int p_sys;
-	int power_counter = 0;
+	uint16_t p_sun;
+	uint16_t p_sys;
+	timestamp_t t;
+
 	for (int i = 0 ; i < eop_length ; i++) {
 		uchar_eop[i] = 0;
 	}
 	vTaskDelay(20 * delay_time_based);
 	while (1) {
 		printf("collecting EOP packet\n");
+		t.tv_sec = 0;
+		t.tv_nsec = 0;
+		obc_timesync(&t, 6000);
+		memcpy(&uchar_eop[0], &t.tv_sec, 4);
+
 		txbuf[0] = 0x88;	//ID = 136 Current ADCS state
 		/* Get Time, Attitude, Position, Velocity information from ADCS */
 		if (i2c_master_transaction_2(0, adcs_node, &txbuf, 1, &rxbuf, 48, adcs_delay) == E_NO_ERR) {
@@ -46,23 +53,33 @@ void EOP_Task(void * pvParameters) {
 		else {
 			printf("(EOP)Error, cannot communicate with ADCS\n");
 		}
-		power_counter ++;
-		if (power_counter == 6) {
+		counter ++;
+		
+		if ((counter % 6) == 1) {
 			/* eps_hk_vi_t */
 			txbuf[0] = 8;
 			txbuf[1] = 1;
-			if (i2c_master_transaction_2(0, eps_node, &txbuf, 2, &rxbuf, 22, adcs_delay) == E_NO_ERR) {
-				memcpy(&vbatt, &rxbuf[10], 2);
-				memcpy(&cursun, &rxbuf[18], 2);
-				memcpy(&cursys, &rxbuf[20], 2);
+			if (i2c_master_transaction_2(0, eps_node, &txbuf, 2, &rxbuf, 22, eps_delay) == E_NO_ERR) {
+				memcpy(&vbatt, &rxbuf[8], 2);
+				memcpy(&cursun, &rxbuf[16], 2);
+				memcpy(&cursys, &rxbuf[18], 2);
+				vbatt = csp_ntoh16(vbatt);
+				cursun = csp_ntoh16(cursun);
+				cursys = csp_ntoh16(cursys);
 				p_sun = (float) cursun * (float) vbatt / 1000.0;
 				p_sys = (float) cursys * (float) vbatt / 1000.0;
+				printf("p_sun = %d\n", p_sun);
+				printf("p_sys = %d\n", p_sys);
 				memcpy(&uchar_eop[28], &p_sun, 2);
 				memcpy(&uchar_eop[30], &p_sys, 2);
 			}
+			counter = 1;
 		}
-		eop_write_dup(uchar_eop);
-		hex_dump(&uchar_eop, eop_length);
+		printf("power_counter = %d\n", counter);
+		eop_write(uchar_eop, 0);
+		eop_write(uchar_eop, 1);
+		// eop_write_dup(uchar_eop);
+		hex_dump(&uchar_eop[0], eop_length);
 		vTaskDelay(10 * delay_time_based);
 	}
 	/** End of init */

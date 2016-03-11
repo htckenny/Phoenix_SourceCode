5b48649653973d834bcfe78b4ab0754cbb3fdb66
Kenny Huang <htckenny@hotmail.com>
2016-03-11 18:08:04 +0800

    Add TC[132-4]
    be abel to change each channel's parameter

--- src/tele_type_132.c
+++ src/tele_type_132.c
@@ -16,6 +16,7 @@
 #define configure 						1
 #define change_mode						2
 #define Execute_sampling_downlink		3
+#define Change_channel_parameter		4
 
 void decodeService132(uint8_t subType, uint8_t*telecommand) {
 
@@ -26,29 +27,29 @@ void decodeService132(uint8_t subType, uint8_t*telecommand) {
 	/* note: parameter start from telecommand[9]*/
 
 	uint16_t packet_length = (telecommand[4] << 8) + telecommand[5] - 4;
-	uint8_t paras[5];
+	uint8_t paras[8];
 	if (packet_length > 0)
-		memcpy(&paras, telecommand + 9, packet_length); 
+		memcpy(&paras, telecommand + 9, packet_length);
 	switch (subType) {
 
 	/*--------------- ID:1 configure ----------------*/
 	case configure:
 		if (packet_length == 2) {
-			sendTelecommandReport_Success(telecommand, CCSDS_S3_ACCEPTANCE_SUCCESS);  
+			sendTelecommandReport_Success(telecommand, CCSDS_S3_ACCEPTANCE_SUCCESS);
 
 			parameters.seuv_period = paras[0];
 			parameters.seuv_sample_rate = paras[1];
 			para_w_flash();
 
-			sendTelecommandReport_Success(telecommand, CCSDS_S3_COMPLETE_SUCCESS); 
-		} 
+			sendTelecommandReport_Success(telecommand, CCSDS_S3_COMPLETE_SUCCESS);
+		}
 		else
-			sendTelecommandReport_Failure(telecommand, CCSDS_T1_ACCEPTANCE_FAIL, CCSDS_ERR_ILLEGAL_TYPE); 
+			sendTelecommandReport_Failure(telecommand, CCSDS_T1_ACCEPTANCE_FAIL, CCSDS_ERR_ILLEGAL_TYPE);
 		break;
 	/*--------------- ID:2 change_mode ----------------*/
 	case change_mode:
-		sendTelecommandReport_Success(telecommand, CCSDS_S3_ACCEPTANCE_SUCCESS);  
-		if (packet_length == 1) {			
+		sendTelecommandReport_Success(telecommand, CCSDS_S3_ACCEPTANCE_SUCCESS);
+		if (packet_length == 1) {
 
 			if (parameters.seuv_mode == 2) {
 				if (paras[0] == 3) {
@@ -63,22 +64,22 @@ void decodeService132(uint8_t subType, uint8_t*telecommand) {
 			else {
 				goto err;
 			}
-			parameters.seuv_mode = paras[0];	//set the seuv mode 
+			parameters.seuv_mode = paras[0];	//set the seuv mode
 			para_w_flash();
 
-			sendTelecommandReport_Success(telecommand, CCSDS_S3_COMPLETE_SUCCESS); 
-			err:
-				completionError = PARA_ERR;
-				sendTelecommandReport_Failure(telecommand, CCSDS_S3_COMPLETE_FAIL, completionError); 
-		} 
+			sendTelecommandReport_Success(telecommand, CCSDS_S3_COMPLETE_SUCCESS);
+err:
+			completionError = PARA_ERR;
+			sendTelecommandReport_Failure(telecommand, CCSDS_S3_COMPLETE_FAIL, completionError);
+		}
 		else
-			sendTelecommandReport_Failure(telecommand, CCSDS_T1_ACCEPTANCE_FAIL, CCSDS_ERR_ILLEGAL_TYPE); 
+			sendTelecommandReport_Failure(telecommand, CCSDS_T1_ACCEPTANCE_FAIL, CCSDS_ERR_ILLEGAL_TYPE);
 
 		break;
 	/*--------------- ID:3 Execute a sampling and downlink ----------------*/
 	case Execute_sampling_downlink:
 		if (packet_length == 0) {
-			sendTelecommandReport_Success(telecommand, CCSDS_S3_ACCEPTANCE_SUCCESS); 
+			sendTelecommandReport_Success(telecommand, CCSDS_S3_ACCEPTANCE_SUCCESS);
 			if (parameters.seuv_mode == 2) {
 				vTaskSuspend(seuv_task);
 			}
@@ -92,9 +93,9 @@ void decodeService132(uint8_t subType, uint8_t*telecommand) {
 			SendDataWithCCSDS_AX25(3, SEUV_data);
 			/* sample SEUV once with gain = 8 */
 			get_a_packet(8);
-			seuvFrame.samples ++; 
+			seuvFrame.samples ++;
 			memcpy(&SEUV_data, &seuvFrame, seuv_length);
-			seuvFrame.samples --; 
+			seuvFrame.samples --;
 			hex_dump(SEUV_data, seuv_length);
 			SendDataWithCCSDS_AX25(3, SEUV_data);
 			if (parameters.seuv_mode == 2) {
@@ -103,13 +104,32 @@ void decodeService132(uint8_t subType, uint8_t*telecommand) {
 			else if (parameters.seuv_mode == 3)
 				power_control(3, OFF);
 
-			sendTelecommandReport_Success(telecommand, CCSDS_S3_COMPLETE_SUCCESS); 
+			sendTelecommandReport_Success(telecommand, CCSDS_S3_COMPLETE_SUCCESS);
 		}
 		else {
 			sendTelecommandReport_Failure(telecommand, CCSDS_S3_COMPLETE_FAIL, completionError);
-		}		
+		}
 		break;
-			
+	/*--------------- ID:4 change channel's parameter ----------------*/
+	case Change_channel_parameter:
+		if (packet_length == 8) {
+			sendTelecommandReport_Success(telecommand, CCSDS_S3_ACCEPTANCE_SUCCESS);
+			/* Gain 1 configuration */
+			parameters.seuv_ch1_G1_conf			= paras[0];
+			parameters.seuv_ch2_G1_conf			= paras[1];
+			parameters.seuv_ch3_G1_conf			= paras[2];
+			parameters.seuv_ch4_G1_conf			= paras[3];
+			/* Gain 8 configuration */
+			parameters.seuv_ch1_G8_conf			= paras[4];
+			parameters.seuv_ch2_G8_conf			= paras[5];
+			parameters.seuv_ch3_G8_conf			= paras[6];
+			parameters.seuv_ch4_G8_conf			= paras[7];
+			para_w_flash();
+			sendTelecommandReport_Success(telecommand, CCSDS_S3_COMPLETE_SUCCESS);
+		}
+		else
+			sendTelecommandReport_Failure(telecommand, CCSDS_T1_ACCEPTANCE_FAIL, CCSDS_ERR_ILLEGAL_TYPE);
+		break;
 	default:
 		sendTelecommandReport_Failure(telecommand, CCSDS_T1_ACCEPTANCE_FAIL, CCSDS_ERR_ILLEGAL_TYPE);
 		break;

b04d30fafb6276a7141dc32b98dc34e2cb9eebb8
Kenny Huang <htckenny@hotmail.com>
2016-03-17 13:56:24 +0800

    Finish the structure of anomaly
    Still need minor adjustment

--- src/task_Anomaly_Monitor.c
+++ src/task_Anomaly_Monitor.c
@@ -22,10 +22,9 @@
 #include "subsystem.h"
 #include "parameter.h"
 
-#define overCurrentThreshold_ADCS_5		100
-#define overCurrentThreshold_ADCS_33	100
-#define overCurrentThreshold_GPS		100
-#define overCurrentThreshold_GPS		100
+#define overCurrentThreshold_ADCS_5		400
+#define overCurrentThreshold_ADCS_33	400
+#define overCurrentThreshold_GPS		400
 
 extern int TS1_4();
 extern int TS5();
@@ -47,52 +46,6 @@ void Anomaly_Monitor_Task(void * pvParameters)
 	int16_t subTemperature_int[11];
 	while (1) {
 
-		txbuf[0] = 0x08;
-		txbuf[1] = 0x02;
-		if (i2c_master_transaction_2(0, eps_node, &txbuf, 2, &rxbuf, 64 + 2, eps_delay) == E_NO_ERR) {
-			memcpy(&subCurrent[0], &rxbuf[2], 2);	// i5	ADCS 	H1-47
-			// memcpy(&subCurrent[1], &rxbuf[4], 2);	// i5	 		H1-49
-			// memcpy(&subCurrent[2], &rxbuf[6], 2);	// i5	SEUV 	H1-51
-			memcpy(&subCurrent[3], &rxbuf[8], 2);	// i3.3	ADCS 	H1-48
-			memcpy(&subCurrent[4], &rxbuf[10], 2);	// i3.3	GPS 	H1-50
-			// memcpy(&subCurrent[5], &rxbuf[12], 2);	// i3.3	SEUV 	H1-52
-		}
-
-		if (subCurrent[0] > overCurrentThreshold_ADCS_5 ) {
-			printf("Out of range %d\n", subCurrent[0]);
-			outRangeCounter_ADCS_5 ++;
-			printf("outCounter = %d\n", outRangeCounter_ADCS_5);
-			if (outRangeCounter_ADCS_5 >= 6) {
-				generate_Error_Report(3, subCurrent[0]);
-			}
-		}
-		else {
-			outRangeCounter_ADCS_5 = 0;
-		}
-
-		if (subCurrent[3] > overCurrentThreshold_ADCS_33 ) {
-			printf("Out of range %d\n", subCurrent[0]);
-			outRangeCounter_ADCS_33 ++;
-			printf("outCounter = %d\n", outRangeCounter_ADCS_33);
-			if (outRangeCounter_ADCS_33 >= 6) {
-				generate_Error_Report(2, subCurrent[3]);
-			}
-		}
-		else {
-			outRangeCounter_ADCS_33 = 0;
-		}
-
-		if (subCurrent[4] > overCurrentThreshold_GPS ) {
-			printf("Out of range %d\n", subCurrent[0]);
-			outRangeCounter_GPS ++;
-			printf("outCounter = %d\n", outRangeCounter_GPS);
-			if (outRangeCounter_GPS >= 6) {
-				generate_Error_Report(4, subCurrent[4]);
-			}
-		}
-		else {
-			outRangeCounter_GPS = 0;
-		}
 		/* OBC Temperature */
 		TS9();
 		subTemperature[0] = ThermalFrame.T9;
@@ -111,6 +64,7 @@ void Anomaly_Monitor_Task(void * pvParameters)
 			outRangeCounter_temp[0] = 0;
 		}
 		/* ----------------------------------------------------- */
+		vTaskDelay(5 * delay_time_based);
 		/* COM Temperature */
 		TS5();
 		subTemperature[1] = ThermalFrame.T5;
@@ -129,6 +83,7 @@ void Anomaly_Monitor_Task(void * pvParameters)
 			outRangeCounter_temp[1] = 0;
 		}
 		/* ----------------------------------------------------- */
+		vTaskDelay(5 * delay_time_based);
 		/* Antenna Temperature */
 		TS6();
 		subTemperature[2] = ThermalFrame.T6;
@@ -146,42 +101,7 @@ void Anomaly_Monitor_Task(void * pvParameters)
 		else {
 			outRangeCounter_temp[2] = 0;
 		}
-		/* ----------------------------------------------------- */
-		/* ADCS ARM Temperature */
-		TS7();
-		subTemperature_int[0] = ThermalFrame.T7;
-		printf("\t\t\ttTemperature %d degree\r\n", subTemperature_int[0]);
-		printf("\E[1A\r");
-
-		/* Operational Temperature Range -10 to +60 */
-		if (subTemperature_int[0] > 60 || subTemperature_int[0] < -10) {
-			outRangeCounter_temp[3] ++;
-			if (outRangeCounter_temp[3] >= 6) {
-				generate_Error_Report(13, subTemperature_int[0]);
-				outRangeCounter_temp[3] = 0;
-			}
-		}
-		else {
-			outRangeCounter_temp[3] = 0;
-		}
-		/* ----------------------------------------------------- */
-		/* ADCS Rate Sensor & Magnetometer Temperature */
-		TS8();
-		subTemperature_int[1] = ThermalFrame.T8;
-		printf("\t\t\ttTemperature %d degree\r\n", subTemperature_int[1]);
-		printf("\E[1A\r");
-
-		/* Operational Temperature Range -10 to +60 */
-		if (subTemperature_int[1] > 60 || subTemperature_int[1] < -10) {
-			outRangeCounter_temp[4] ++;
-			if (outRangeCounter_temp[4] >= 6) {
-				generate_Error_Report(14, subTemperature_int[1]);
-				outRangeCounter_temp[4] = 0;
-			}
-		}
-		else {
-			outRangeCounter_temp[4] = 0;
-		}
+		vTaskDelay(5 * delay_time_based);
 		/* ----------------------------------------------------- */
 		/* EPS Temperature */
 		TS1_4();
@@ -214,6 +134,97 @@ void Anomaly_Monitor_Task(void * pvParameters)
 			outRangeCounter_temp[6] = 0;
 		}
 		/* ----------------------------------------------------- */
+		vTaskDelay(5 * delay_time_based);
+		if (adcs_task != NULL) {
+
+
+			txbuf[0] = 0x08;
+			txbuf[1] = 0x02;
+			if (i2c_master_transaction_2(0, stm_eps_node, &txbuf, 2, &rxbuf, 64 + 2, eps_delay) == E_NO_ERR) {
+				memcpy(&subCurrent[0], &rxbuf[2], 2);	// i5	ADCS 	H1-47
+				// memcpy(&subCurrent[1], &rxbuf[4], 2);	// i5	 		H1-49
+				// memcpy(&subCurrent[2], &rxbuf[6], 2);	// i5	SEUV 	H1-51
+				memcpy(&subCurrent[3], &rxbuf[8], 2);	// i3.3	ADCS 	H1-48
+				memcpy(&subCurrent[4], &rxbuf[10], 2);	// i3.3	GPS 	H1-50
+				// memcpy(&subCurrent[5], &rxbuf[12], 2);	// i3.3	SEUV 	H1-52
+			}
+
+			if (subCurrent[0] > overCurrentThreshold_ADCS_5 ) {
+				printf("Out of range %d\n", subCurrent[0]);
+				outRangeCounter_ADCS_5 ++;
+				printf("outCounter = %d\n", outRangeCounter_ADCS_5);
+				if (outRangeCounter_ADCS_5 >= 6) {
+					generate_Error_Report(3, subCurrent[0]);
+				}
+			}
+			else {
+				outRangeCounter_ADCS_5 = 0;
+			}
+
+			if (subCurrent[3] > overCurrentThreshold_ADCS_33 ) {
+				printf("Out of range %d\n", subCurrent[0]);
+				outRangeCounter_ADCS_33 ++;
+				printf("outCounter = %d\n", outRangeCounter_ADCS_33);
+				if (outRangeCounter_ADCS_33 >= 6) {
+					generate_Error_Report(2, subCurrent[3]);
+				}
+			}
+			else {
+				outRangeCounter_ADCS_33 = 0;
+			}
+
+			if (subCurrent[4] > overCurrentThreshold_GPS ) {
+				printf("Out of range %d\n", subCurrent[0]);
+				outRangeCounter_GPS ++;
+				printf("outCounter = %d\n", outRangeCounter_GPS);
+				if (outRangeCounter_GPS >= 6) {
+					generate_Error_Report(4, subCurrent[4]);
+				}
+			}
+			else {
+				outRangeCounter_GPS = 0;
+			}
+			/* ----------------------------------------------------- */
+			vTaskDelay(5 * delay_time_based);
+			/* ADCS ARM Temperature */
+			TS7();
+			subTemperature_int[0] = ThermalFrame.T7;
+			printf("\t\t\ttTemperature %d degree\r\n", subTemperature_int[0]);
+			printf("\E[1A\r");
+
+			/* Operational Temperature Range -10 to +60 */
+			if (subTemperature_int[0] > 60 || subTemperature_int[0] < -10) {
+				outRangeCounter_temp[3] ++;
+				if (outRangeCounter_temp[3] >= 6) {
+					generate_Error_Report(13, subTemperature_int[0]);
+					outRangeCounter_temp[3] = 0;
+				}
+			}
+			else {
+				outRangeCounter_temp[3] = 0;
+			}
+			/* ----------------------------------------------------- */
+			vTaskDelay(5 * delay_time_based);
+			/* ADCS Rate Sensor & Magnetometer Temperature */
+			TS8();
+			subTemperature_int[1] = ThermalFrame.T8;
+			printf("\t\t\ttTemperature %d degree\r\n", subTemperature_int[1]);
+			printf("\E[1A\r");
+
+			/* Operational Temperature Range -10 to +60 */
+			if (subTemperature_int[1] > 60 || subTemperature_int[1] < -10) {
+				outRangeCounter_temp[4] ++;
+				if (outRangeCounter_temp[4] >= 6) {
+					generate_Error_Report(14, subTemperature_int[1]);
+					outRangeCounter_temp[4] = 0;
+				}
+			}
+			else {
+				outRangeCounter_temp[4] = 0;
+			}
+			/* ----------------------------------------------------- */
+			vTaskDelay(5 * delay_time_based);
+		}
 
 		vTaskDelay(5 * delay_time_based);
 	}
